1aa6f0c87547df5587d09af1ec79bf939ae8e9a3
Marco-PC\Marco <epossmurfmarco@gmail.com>
2023-04-17 14:48:52 +0200

    changing folder name

---
+++ backend/src/main/kotlin/nel/marco/api/v1/FindingTaskController.kt
@@ -0,0 +1,42 @@
+package nel.marco.api.v1
+
+import nel.marco.api.v1.model.TaskModel
+import nel.marco.service.TaskService
+import nel.marco.service.dto.mapToModel
+import org.springframework.format.annotation.DateTimeFormat
+import org.springframework.http.MediaType.ALL_VALUE
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
+
+@RestController
+@RequestMapping(path = ["/v1"], consumes = [ALL_VALUE], produces = [APPLICATION_JSON_VALUE])
+class FindingTaskController(
+    private val taskService: TaskService
+) {
+
+    @GetMapping("/task")
+    fun findAllTasks(
+        @RequestParam(required = false) ids: List<Long>? = null,
+        @RequestParam(required = false) message: String? = null,
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam(required = false) isBefore: LocalDate? = null,
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam(required = false) isAfter: LocalDate? = null,
+        @RequestParam(required = false) completed: Boolean? = null,
+    ): List<TaskModel> {
+        val findAll =
+            taskService.findAll(ids, message, isBefore = isBefore, isAfter = isAfter, completed = completed)
+        return findAll.map { it.mapToModel() }
+    }
+
+    @GetMapping("/task/{id}")
+    fun findTask(@PathVariable id: Long): ResponseEntity<TaskModel> {
+        val find = taskService.find(id)
+
+        if (find == null)
+            return ResponseEntity.notFound().build()
+
+        return ResponseEntity.ok(find.mapToModel())
+    }
+
+}
---
+++ backend/src/test/kotlin/nel/marco/api/ModifyingTaskControllerTest.kt
@@ -0,0 +1,89 @@
+package nel.marco.api
+
+
+import nel.marco.api.v1.ModifyingTaskController
+import nel.marco.api.v1.model.CreateTaskRequest
+import nel.marco.api.v1.model.TaskModel
+import nel.marco.db.Task
+import nel.marco.db.TaskJpaRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.Rollback
+import org.springframework.transaction.annotation.Transactional
+
+@SpringBootTest
+@Rollback
+@Transactional
+internal class ModifyingTaskControllerTest {
+
+    @Autowired
+    lateinit var taskJpaRepository: TaskJpaRepository
+
+    @Autowired
+    lateinit var modifyingTaskController: ModifyingTaskController
+
+    @Test
+    fun `create default task and expect task to be in db`() {
+        modifyingTaskController.createTask()
+
+        assertThat(taskJpaRepository.findAll()).isNotEmpty
+    }
+
+    @Test
+    fun `testCreateTask with custom message`() {
+        val customMessage = "Marco!!!!!"
+        modifyingTaskController.createTask(CreateTaskRequest(customMessage))
+
+        val tasks = taskJpaRepository.findAll()
+        val task = tasks.find { it.message == customMessage }
+        assertThat(task?.message).isEqualTo(customMessage)
+
+    }
+
+    @Test
+    fun `delete saved task`() {
+        val save = taskJpaRepository.save(Task())
+
+        val actual = modifyingTaskController.deleteTask(save.id)
+
+        assertThat(actual.statusCode.is2xxSuccessful).isTrue()
+    }
+
+    @Test
+    fun `delete task that does not exist`() {
+        val actual = modifyingTaskController.deleteTask(999999999)
+
+        assertThat(actual.statusCode.is2xxSuccessful).isTrue
+    }
+
+
+    @Test
+    fun `update task that does not exist expect Exception`() {
+        assertThrows<RuntimeException> {
+            modifyingTaskController.updateTask(TaskModel(id = 1, message = "failure"))
+        }
+    }
+
+    @Test
+    fun `update task that exist`() {
+        modifyingTaskController.createTask()
+        val createTask = modifyingTaskController.createTask()
+        val updatedTask = modifyingTaskController.updateTask(TaskModel(id = createTask.id, message = "changed"))
+
+        assertThat(updatedTask.id).isEqualTo(createTask.id)
+        assertThat(updatedTask.message).isEqualTo("changed")
+    }
+
+
+    @Test
+    fun `delete task that exist`() {
+        val createTask = modifyingTaskController.createTask()
+        val deleteTask = modifyingTaskController.deleteTask(createTask.id)
+
+        assertThat(deleteTask.statusCode.is2xxSuccessful).isTrue()
+    }
+
+}
