bde4fb5175c5d11745473a53dfb5658f374cb344
mnel <mnel@bol.com>
2023-07-31 16:20:28 +0200

    Adding angular code without styling

---
+++ Angular/src/app/TaskService.ts
@@ -0,0 +1,56 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from '@angular/common/http';
+import {Observable} from "rxjs";
+
+
+@Injectable()
+export class TaskService {
+    constructor(private http: HttpClient) {
+    }
+
+    public fetchTask(filterText: string, isCompleted: boolean | null): Observable<Task[]> {
+        let baseUrl = `http://localhost:8090/v1/task`;
+
+        if (isCompleted == null) {
+            return this.http.get<Array<Task>>(`${baseUrl}?message=${filterText}`)
+        }
+
+        return this.http.get<Array<Task>>(`${baseUrl}?message=${filterText}&completed=${isCompleted}`)
+    }
+
+    public updateTask(task: Task): Observable<Object> {
+        return this.http.put(
+            `http://localhost:8090/v1/task`,
+            task
+        )
+    }
+
+    public removeTask(task: Task): Observable<Object> {
+        return this.http.delete(
+            `http://localhost:8090/v1/task/${task.id}`
+        )
+    }
+
+    public createTask(): Observable<Object> {
+        return this.http.get(`http://localhost:8090/v1/create`)
+    }
+}
+
+
+export class Task {
+    id: string | "" = "0"
+    message: string | "" = ""
+    created: string | null = null
+    updated: string | null = null
+    completed: boolean | false = false
+
+    constructor(id: string | "", message: string | "") {
+        this.id = id;
+        this.message = message;
+    }
+
+}
+
+export class CreateTaskRequest {
+    message: string | "" = ""
+}
---
+++ Angular/src/app/app.component.ts
@@ -0,0 +1,87 @@
+import {Component, Injectable, OnInit} from '@angular/core';
+import {Task, TaskService} from "./TaskService";
+import {FormControl, FormGroup} from "@angular/forms";
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+@Injectable()
+export class AppComponent implements OnInit {
+    title = 'my-app';
+    tasks: Task[] = []
+    filterText: string = ""
+    isCompleted: boolean | null = null
+    isCompletedForm: any;
+
+    constructor(private taskService: TaskService) {
+    }
+
+    ngOnInit(): void {
+        let filterText = ''
+        let isCompleted = false
+
+        this.isCompletedForm = new FormGroup({
+            isCompleted: new FormControl(),
+        });
+
+        this.updateTasks()
+    }
+
+    public updateTasks() {
+        console.log(`calling ${this.filterText} ${this.isCompleted}`)
+        this.taskService.fetchTask(this.filterText, this.isCompleted)
+            .subscribe((data) => {
+                this.tasks = data.map(item => item);
+            })
+    }
+
+    public clear() {
+        this.filterText = ''
+        this.updateTasks()
+    }
+
+
+    public completeTask(task: Task) {
+        task.completed = !task.completed
+        this.taskService.updateTask(task)
+            .subscribe(response => {
+                this.updateTasks()
+            })
+    }
+
+    public updateSpecificTask(task: Task, message: string) {
+        task.message = message
+        this.taskService.updateTask(task)
+            .subscribe(response => {
+                this.updateTasks()
+            })
+    }
+
+    public removeSpecificTask(task: Task) {
+        console.log(`removeSpecificTask`, task)
+        this.taskService.removeTask(task)
+            .subscribe(response => {
+                this.updateTasks()
+            })
+    }
+
+    public createNewTask() {
+        this.taskService.createTask()
+            .subscribe(response => {
+                this.updateTasks()
+            })
+    }
+
+    public updateFilter(value: string) {
+        if(value == '')
+            this.isCompleted = null
+        else if(value == 'true')
+            this.isCompleted = true
+        else if(value == 'false')
+            this.isCompleted = false
+
+        this.updateTasks()
+    }
+}
