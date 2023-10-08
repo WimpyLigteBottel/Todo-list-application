1625465335cd657065a99db02953164f0b7c4c19
Marco-PC\Marco <epossmurfmarco@gmail.com>
2023-10-08 21:58:32 +0200

    Test deploy netlify

--- React/src/application/App.jsx
+++ React/src/application/App.jsx
@@ -3,54 +3,61 @@ import Search from "./search/Search";
 import Radio from "./radio/Radio";
 import Task from "./task/Task";
 import AddingButton from "./adding/AddingButton";
-import {fetchTasks,} from "./core/TaskService";
-import {useEffect, useState} from "react";
+import { fetchTasks } from "./core/TaskService";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-    const [doCall, setDoCall] = useState(false);
-    const [tasks, setTasks] = useState([]);
-    const [selectedOption, setSelectedOption] = useState("");
-    const [filterText, setFilterText] = useState("");
+  const [doCall, setDoCall] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [filterText, setFilterText] = useState("");
 
-    useEffect(() => {
-        if (!doCall) {
-            return
-        }
-        fetchTasks(filterText, selectedOption)
-            .then((response => {
-                setTasks(response);
-                setDoCall(false)
-            }));
-    }, [selectedOption, filterText, doCall]);
+  axios
+    .get("http://localhost:3000/hello")
+    .then((response) => {
+      console.log(response.data, "TESTING");
+      return response.data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
-    return (
-        <div>
-            <Radio
-                selectedOption={selectedOption}
-                callbackSelectedOption={(text) => {
-                    setSelectedOption(text)
-                    setDoCall(true)
-                }}
-            />
+  useEffect(() => {
+    if (!doCall) {
+      return;
+    }
+    fetchTasks(filterText, selectedOption).then((response) => {
+      setTasks(response);
+      setDoCall(false);
+    });
+  }, [selectedOption, filterText, doCall]);
 
-            {/* search button */}
-            <Search
-                filterText={filterText}
-                onFilterChange={(value) => {
-                    setFilterText(value)
-                    setDoCall(true)
-                }}
-                callbackSearch={() => setDoCall(true)}
-            />
+  return (
+    <div>
+      <Radio
+        selectedOption={selectedOption}
+        callbackSelectedOption={(text) => {
+          setSelectedOption(text);
+          setDoCall(true);
+        }}
+      />
 
-            <Task
-                tasks={tasks}
-                callbackUpdateParentTasks={() => setDoCall(true)}
-            />
+      {/* search button */}
+      <Search
+        filterText={filterText}
+        onFilterChange={(value) => {
+          setFilterText(value);
+          setDoCall(true);
+        }}
+        callbackSearch={() => setDoCall(true)}
+      />
 
-            <AddingButton callbackAddTask={() => setDoCall(true)}/>
-        </div>
-    );
+      <Task tasks={tasks} callbackUpdateParentTasks={() => setDoCall(true)} />
+
+      <AddingButton callbackAddTask={() => setDoCall(true)} />
+    </div>
+  );
 }
 
 export default App;
